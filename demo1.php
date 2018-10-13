fc8b33d7081652b595a107262aa17b985c97e3f5
Lsunshineandrainbow <3480782290@qq.com>
2018-10-13 15:39:36 +0800

    W3基础书写模式

---
+++ demo1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$color="信小呆";
+echo "我是区分大小写的";
+Echo "中国锦鲤";
+echo "字符串"."i凝结";
+$forunt = array("中国","锦鲤");
+$x="我是";
+$z = $x.$forunt[0].$forunt[1];
+echo $z;
+?>
+</body>
+</html>
+
+
+
+
