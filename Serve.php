d873ee07108a773c103a692d70d4465592fef5e7
Lsunshineandrainbow <3480782290@qq.com>
2018-10-14 13:39:18 +0800

    PHP中表单例子后端服务模拟

---
+++ Serve.php
@@ -0,0 +1,15 @@
+<?php
+//echo '后端Serve页面';
+//$Username = $_GET['username'];
+//$pwd = $_GET['password'];
+//print ('<br>');
+//echo '用户名'.$Username,"密码".$pwd;
+$ONE = $_POST['sex'];
+$Many = $_POST['hobby'];
+$Select = $_POST['room'];
+echo $ONE;
+echo '<br>';
+var_dump($Many);
+echo '<br>';
+var_dump($Select);
+
