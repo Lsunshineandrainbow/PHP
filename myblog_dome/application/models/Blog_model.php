b0234d4cbcee85e319d7ce2ef5318b8341c2c2c1
Lsunshineandrainbow <3480782290@qq.com>
2018-11-05 23:08:52 +0800

    ci框架  个人博客

--- myblog_dome/application/models/Blog_model.php
+++ myblog_dome/application/models/Blog_model.php
@@ -3,7 +3,19 @@
     {
         public function get_t_log()
         {
-            $query = $this->db->get('t_blog');      //查表
+//            $query = $this->db->get('t_blog');      //查表
+//            return $query->result();
+            $this->db->select('*');
+            $this->db->from('t_blog a');
+            $this->db->join('t_blog_catalog b','a.catalog_id=b.catalog_id' );
+            $this->db->order_by('a.post_time','desc');
+            $query = $this->db->get();
+
+            return $query->result();
+
+        }
+        public function get_t_blog_catalog(){
+            $query = $this->db->get('t_blog_catalog');
             return $query->result();
         }
     }
